1d38142b2bd6154d6e43e435d85b2d5d12c5ea55
Marius Meschter <git@onestay.moe>
2022-07-13 23:51:59 +0200

    make use of the threadpool in server.rs

--- src/server.rs
+++ src/server.rs
@@ -1,20 +1,24 @@
+use crate::connection::HttpConnection;
+use crate::error::Error;
+use crate::threadpool::ThreadPool;
 use std::net::{SocketAddr, TcpListener};
 use std::str::FromStr;
-use std::thread;
-use crate::error::Error;
-use crate::connection::HttpConnection;
 
 pub struct Server {
     socket: Option<TcpListener>,
     socket_addr: SocketAddr,
+    threadpool: ThreadPool,
 }
+
+const NUM_THREADS: usize = 10;
 
 impl Server {
     pub fn new(addr: &str) -> Result<Self, Error> {
         let socket_addr = SocketAddr::from_str(addr)?;
         Ok(Server {
             socket: None,
-            socket_addr
+            socket_addr,
+            threadpool: ThreadPool::new(NUM_THREADS),
         })
     }
 
@@ -23,13 +27,9 @@ impl Server {
 
         loop {
             let (stream, addr) = self.socket.as_ref().expect("socket is none").accept()?;
-            
-            thread::spawn(move || {
+            self.threadpool.execute(move || {
                 HttpConnection::init(stream);
-
             });
         }
     }
 }
-
-
